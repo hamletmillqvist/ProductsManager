87e04678289ad5796a5943d7d7cd2cf23f7c3aae
valrossenOliver <valrossen.oliver@protonmail.ch>
2024-12-10 00:10:07 +0100

    MariaDB running locally, stared getting ORM up and running

--- src/Server/Entities/Product.ts
+++ src/Server/Entities/Product.ts
@@ -1,6 +1,16 @@
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+
+@Entity()
 export class Product {
+    @PrimaryGeneratedColumn()
     id!: number;
+
+    @Column({type:"string", unique: true})
     name!: string;
+
+    @Column({type:"string", nullable: true})
     description!: string | null;
+
+    @Column({type: "decimal"})
     price!: number;
 }
--- src/Server/Services/ProductService.ts
+++ src/Server/Services/ProductService.ts
@@ -1,16 +1,23 @@
 import ProductModel from "@models/ProductModel";
 import {Product} from "@entities/Product";
 import NotFoundError from "@errors/NotFoundError";
+import MySqlDataSource from "../MySqlDataSource";
 
 export default class ProductService {
+
+    private productRepository = MySqlDataSource.getRepository(Product);
 
     async getProductList(searchTerm?: string | null): Promise<ProductModel[]> {
         return [];
     }
 
     async getProduct(id: number): Promise<ProductModel | null> {
-        throw new NotFoundError("Could not find Product with id: " + id);
-        const product = new Product(); // todo
+        const product = await this.productRepository.findOneBy({id: id});
+
+        if (!product) {
+            throw new NotFoundError("Could not find Product with id: " + id);
+        }
+
         return {
             id: product.id,
             name: product.name,
